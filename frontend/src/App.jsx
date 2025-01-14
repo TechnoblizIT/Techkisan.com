1eedd7cb29ccac7307c6fc7f3c841d7b7b6c1ac7
geetanshpardhi1 <geetanshpardhi@gmail.com>
2025-01-14 17:41:35 +0530

    added services section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Hero from "./components/Hero/Hero";
+import Services from "./components/Services/Services";
 
 
 function App() {
   return (
     <>
       <Hero />
+      <Services />
       <Outlet />
     </>
   );
---
+++ frontend/src/components/Services/Services.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import homeautomation from "../../assets/home-automation.png"
+import solarservice from "../../assets/solar-energy.png"
+import securitysol from "../../assets/security.png"
+import building from "../../assets/building-automation.png"
+import itservice from "../../assets/app-development.png"
+import officefactory from "../../assets/sensor.png"
+import { motion } from "framer-motion";
+
+const ServicesData = [
+  {
+    id: 1,
+    title: "Home Automation",
+    link: "#",
+    image: homeautomation,
+    delay: 0.2,
+  },
+  {
+    id: 2,
+    title: "Solar Services",
+    link: "#",
+    image: solarservice,
+    delay: 0.3,
+  },    
+  {
+    id: 3,
+    title: "Security Solutions",
+    link: "#",
+    image: securitysol,
+    delay: 0.4,
+  },
+  {
+    id: 4,
+    title: "Building & Commercial Automation",
+    link: "#",
+    image: building,
+    delay: 0.5,
+  },
+  {
+    id: 5,
+    title: "IT Services",
+    link: "#",
+    image: itservice,
+    delay: 0.6,
+  },
+  {
+    id: 6,
+    title: "Office & Factory Automation",
+    link: "#",
+    image: officefactory,
+    delay: 0.7,
+  },
+];
+
+const SlideLeft = (delay) => {
+  return {
+    initial: {
+      opacity: 0,
+      x: 50,
+    },
+    animate: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.3,
+        delay: delay,
+        ease: "easeInOut",
+      },
+    },
+  };
+};
+const Services = () => {
+  return (
+    <section className="bg-white">
+      <div className="container pb-14 pt-5">
+        <h1 className="text-4xl font-bold text-left pb-10 text-center">
+          Services we provide
+        </h1>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-8">
+          {ServicesData.map((service) => (
+            <motion.div
+              variants={SlideLeft(service.delay)}
+              initial="initial"
+              whileInView={"animate"}
+              viewport={{ once: true }}
+              className="bg-[#f4f4f4] rounded-2xl flex flex-col gap-4 items-center justify-center p-4 py-7 hover:bg-white hover:scale-110 duration-300 hover:shadow-2xl"
+            >
+              <div className="text-4xl mb-4"><img src={service.image} alt="image" className="h-20 w-20"/></div>
+              <h1 className="text-lg font-semibold text-center px-3">
+                {service.title}
+              </h1>
+            </motion.div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
