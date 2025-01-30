68bef33f1f90c94296c85c5f98d09dbd6c57b4b6
geetanshpardhi1 <geetanshpardhi@gmail.com>
2025-01-30 20:16:31 +0530

    some bug fixes in signinsingup form

--- frontend/src/components/SignInSignUp/SignInSignUp.jsx
+++ frontend/src/components/SignInSignUp/SignInSignUp.jsx
@@ -1,8 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../../styles/SignInSignUp.module.css";
 
 const SignInSignUp = () => {
   const [isSignUpMode, setIsSignUpMode] = useState(false);
+  const [showForm, setShowForm] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowForm(isSignUpMode);
+    }, 700);
+
+    return () => clearTimeout(timeout);
+  }, [isSignUpMode]);
 
   const handleSignUpClick = () => {
     setIsSignUpMode(true);
@@ -20,55 +29,28 @@ const SignInSignUp = () => {
     >
       <div className={styles["forms-container"]}>
         <div className={styles["signin-signup"]}>
-          {!isSignUpMode ? (
-            <form
-              className={`${styles["sign-in-form"]} ${
-                !isSignUpMode ? styles["active"] : ""
-              }`}
-              onSubmit=""
-            >
-              <h2 className={styles.title}>Sign in</h2>
+          {showForm ? (
+            <form className={styles["sign-up-form"]} onSubmit="">
+              <h2 className={styles.title}>Sign up</h2>
 
               <div className={styles["input-field"]}>
                 <i className="fas fa-user"></i>
-
-                <input
-                  required
-                  type="text"
-                  placeholder="Username"
-                  value=""
-                  onChange=""
-                />
+                <input required type="text" placeholder="Email" />
               </div>
               <div className={styles["input-field"]}>
                 <i className="fas fa-lock"></i>
-                <input
-                  required
-                  type="password"
-                  placeholder="Password"
-                  value=""
-                  onChange=""
-                />
+                <input required type="password" placeholder="Password" />
               </div>
-                <input
-                  type="submit"
-                  value="Login"
-                  className="primary-btn m-2"
-                />
+              <div className={styles["input-field"]}>
+                <i className="fas fa-lock"></i>
+                <input required type="password" placeholder="Confirm Password" />
+              </div>
 
-              <button
-                type="button"
-                className="text-black-500 cursor-pointer transition duration-300 ease-in-out hover:text-blue-700 hover:glow hover:shadow-blue-500/50"
-                onClick=""
-              >
-                Forgot Password ?
-              </button>
+              <input type="submit" className="primary-btn m-2" value="Sign up" />
 
-              <p className={styles["social-text"]}>
-                Or Sign in with social platforms
-              </p>
+              <p className={styles["social-text"]}>Or Sign up with social platforms</p>
               <div className={styles["social-media"]}>
-                {['facebook-f', 'twitter', 'google', 'linkedin-in'].map((icon) => (
+                {["facebook-f", "twitter", "google", "linkedin-in"].map((icon) => (
                   <a key={icon} href="#" className={styles["social-icon"]}>
                     <i className={`fab fa-${icon}`}></i>
                   </a>
@@ -76,52 +58,27 @@ const SignInSignUp = () => {
               </div>
             </form>
           ) : (
-            <form
-              className={`${styles["sign-up-form"]} ${
-                isSignUpMode ? styles["active"] : ""
-              }`}
-              onSubmit=""
-            >
-              <h2 className={styles.title}>Sign up</h2>
+            <form className={styles["sign-in-form"]} onSubmit="">
+              <h2 className={styles.title}>Sign in</h2>
 
               <div className={styles["input-field"]}>
                 <i className="fas fa-user"></i>
-                <input
-                  required
-                  type="text"
-                  placeholder="Email"
-                  value=""
-                  onChange=""
-                />
+                <input required type="text" placeholder="Username" />
               </div>
               <div className={styles["input-field"]}>
                 <i className="fas fa-lock"></i>
-                <input
-                  required
-                  type="password"
-                  placeholder="Password"
-                  value=""
-                  onChange=""
-                />
-              </div>
-              <div className={styles["input-field"]}>
-                <i className="fas fa-lock"></i>
-                <input
-                  required
-                  type="password"
-                  placeholder="Confirm Password"
-                  value=""
-                  onChange=""
-                />
+                <input required type="password" placeholder="Password" />
               </div>
 
-                <input type="submit" className="primary-btn m-2" value="Sign up" />
-           
-              <p className={styles["social-text"]}>
-                Or Sign up with social platforms
-              </p>
+              <input type="submit" value="Login" className="primary-btn m-2" />
+
+              <button type="button" className="text-black-500 hover:text-blue-700">
+                Forgot Password?
+              </button>
+
+              <p className={styles["social-text"]}>Or Sign in with social platforms</p>
               <div className={styles["social-media"]}>
-                {['facebook-f', 'twitter', 'google', 'linkedin-in'].map((icon) => (
+                {["facebook-f", "twitter", "google", "linkedin-in"].map((icon) => (
                   <a key={icon} href="#" className={styles["social-icon"]}>
                     <i className={`fab fa-${icon}`}></i>
                   </a>
@@ -136,14 +93,8 @@ const SignInSignUp = () => {
         <div className={`${styles["panel"]} ${styles["left-panel"]}`}>
           <div className={styles.content}>
             <h3>New here?</h3>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
-              ex ratione. Aliquid!
-            </p>
-            <button
-              className={`${styles.btn} ${styles.transparent}`}
-              onClick={handleSignUpClick}
-            >
+            <p>Join us today to access premium features!</p>
+            <button className={`${styles.btn} ${styles.transparent}`} onClick={handleSignUpClick}>
               Sign up
             </button>
           </div>
@@ -152,14 +103,8 @@ const SignInSignUp = () => {
         <div className={`${styles["panel"]} ${styles["right-panel"]}`}>
           <div className={styles.content}>
             <h3>One of us?</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-              laboriosam ad deleniti.
-            </p>
-            <button
-              className={`${styles.btn} ${styles.transparent}`}
-              onClick={handleSignInClick}
-            >
+            <p>Sign in to continue your journey.</p>
+            <button className={`${styles.btn} ${styles.transparent}`} onClick={handleSignInClick}>
               Sign in
             </button>
           </div>
